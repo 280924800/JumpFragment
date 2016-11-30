20d00731deb62890eafa7c951ce5d55f3f80f98e
xin <280924800@qq.com>
2016-11-30 21:08:38 +0800

    测试

--- src/main/java/com/example/xin/hello/MainActivity.java
+++ src/main/java/com/example/xin/hello/MainActivity.java
@@ -1,72 +1,19 @@
 package com.example.xin.hello;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-import java.io.IOException;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextView;
-    private Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
 
-            mTextView.setText(msg.obj+"");
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.textview);
-        mTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getData();
-            }
-        });
-
-
-    }
-
-    private void reUI(String str) {
-       Message msg = mHandler.obtainMessage();
-        msg.obj = str;
-        mHandler.sendMessage(msg);
-    }
-
-    public void getData() {
-        //创建okHttpClient对象
-        OkHttpClient mOkHttpClient = new OkHttpClient();
-        //创建一个Request
-        final Request request = new Request.Builder()
-                .url("http://192.168.56.101:8080/Test/test")
-                .build();
-        //new call
-        Call call = mOkHttpClient.newCall(request);
-        //请求加入调度
-        call.enqueue(new okhttp3.Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String htmlStr = response.body().string();
-                if (htmlStr != null) {
-                    reUI(htmlStr);
-                }
-            }
-        });
+        getFragmentManager().
+                beginTransaction().
+                addToBackStack(null).
+                replace(R.id.content, new OneFragment()).
+                commit();
     }
 }
